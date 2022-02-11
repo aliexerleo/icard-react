a685ada49523e7f2ee8587bc4de300ab54a8b326
aliexerleo <aliexermayor@hotmail.com>
2022-02-10 22:25:13 -0500

    nuevo endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_API = "http://127.0.0.1:8000";
+//export const BASE_API = "http://127.0.0.1:8000";
+export const BASE_API = "http://44.202.52.205";
 export const TOKEN = "token";
 
 export const ORDER_STATUS = {
